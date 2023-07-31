442aa334040551e1d5a10f3d37e242be50cd8339
dev-david-alves <davidalves9090@gmail.com>
2023-07-31 20:52:38 -0300

    Project updated

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -2,8 +2,7 @@ export const exerciseOptions = {
     method: "GET",
     headers: {
         "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-        "X-RapidAPI-Key": "43128831d2msh6c9edd68097a267p1545e3jsn7658546f7215",
-        // "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     },
 };
 
